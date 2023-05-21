8d7de73b38f2a18139471d1ca37f095c2e2413dc
mohamedabdelrahman <mohamed.abdelrahman@bld.ai>
2023-05-20 22:55:52 -0500

    Added NotesDataCubit provider and consumer

--- nota/lib/components/note_list.dart
+++ nota/lib/components/note_list.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
-import '../models/note_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nota/cubits/notes_data_cubit/cubit.dart';
+import 'package:nota/cubits/notes_data_cubit/states.dart';
 import 'note_tile.dart';
 
 class NotesList extends StatelessWidget {
-  NotesList({super.key});
-  final listTiles = [
-    for (final object in NoteModel.createDummyList()) NoteTile(note: object)
-  ];
+  const NotesList({super.key});
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: DraggableScrollableSheet(
-        initialChildSize: 1,
-        builder: (context, scrollController) => ListView.builder(
-          controller: scrollController,
-          itemCount: listTiles.length,
-          itemBuilder: (BuildContext context, int index) => listTiles[index],
-        ),
-      ),
+    final cubit = NotesDataCubit().get(context);
+    return BlocConsumer<NotesDataCubit, NotesDataState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        return SizedBox.expand(
+          child: DraggableScrollableSheet(
+            initialChildSize: 1,
+            builder: (context, scrollController) => ListView.builder(
+              controller: scrollController,
+              itemCount: cubit.notesList.length,
+              itemBuilder: (BuildContext context, int index) =>
+                  NoteTile(note: cubit.notesList[index], index: index),
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- nota/lib/screens/home_screen.dart
+++ nota/lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nota/components/note_list.dart';
+import 'package:nota/cubits/notes_data_cubit/cubit.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,7 +14,10 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       backgroundColor: Colors.white70,
-      body: NotesList(),
+      body: BlocProvider(
+        create: (context) => NotesDataCubit(),
+        child: const NotesList(),
+      ),
     );
   }
 }
