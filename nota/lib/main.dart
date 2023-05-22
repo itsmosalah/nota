e7c467893cf2e08a7390fcb938dbfe145917687d
mohamedabdelrahman <mohamed.abdelrahman@bld.ai>
2023-05-22 00:26:35 -0500

    Move NotesDataCubit to above MaterialApp
    - Make it accessible across multiple routes, which is required.
    
    ref #2

--- nota/lib/main.dart
+++ nota/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nota/cubits/notes_data_cubit/cubit.dart';
 import 'package:nota/screens/home_screen.dart';
 
 void main() {
@@ -11,14 +13,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Nota',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => NotesDataCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Nota',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
       ),
-      home: const HomeScreen(),
     );
   }
 }
