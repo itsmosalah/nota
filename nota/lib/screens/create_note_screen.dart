fa06f512aa1503b237e0991d24f50e1b6c9d1d78
mohamedabdelrahman <mohamed.abdelrahman@bld.ai>
2023-05-23 11:21:05 -0500

    Create NewNote class and WillPopScopeDialogue widget
    - Separate required logic to create new note in NewNote class
    - Use the NewNote object in create_note_screen
    - Create widget WillPopScopeDialogue for showing dialogue on hitting back
    
    ref #2

--- nota/lib/screens/create_note_screen.dart
+++ nota/lib/screens/create_note_screen.dart
@@ -1,52 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:nota/components/new_note.dart';
 import 'package:nota/components/note_scaffold.dart';
-import 'package:nota/cubits/notes_data_cubit/cubit.dart';
-import 'package:nota/models/note_model.dart';
+import 'package:nota/components/will_pop_scope_dialogue.dart';
 
 class CreateNoteScreen extends StatelessWidget {
-  CreateNoteScreen({super.key});
-
-  final focusNode = FocusNode();
-  final textController = TextEditingController();
+  const CreateNoteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create new note'),
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        actions: [
-          IconButton(
-            onPressed: () {
-              // TODO: create saveNoteModal that confirms name and saves
-              //  - Implement name text field with default value
-              //  - then pass the note to createNote once done
-              NotesDataCubit.get(context).createNote(
-                  note: NoteModel(
-                title: 'New Note',
-                content: textController.text,
-              ));
-            },
-            icon: const Icon(Icons.check),
-          )
-        ],
-      ),
-      backgroundColor: Colors.white70,
-      body: NoteScaffold(
-        child: InkWell(
-          onTap: () => focusNode.requestFocus(),
-          child: Column(
-            children: [
-              TextField(
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                decoration: null,
-                autofocus: true,
-                focusNode: focusNode,
-                controller: textController,
-              ),
-            ],
-          ),
+    final NewNoteUI newNote = NewNoteUI(context: context);
+
+    return WillPopScopeDialogue(
+      header: 'Are you sure?',
+      textPrompt: 'Exiting will discard the changes.',
+      yesActionText: 'Discard',
+      noActionText: 'Continue editing',
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Create new note'),
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          actions: [newNote.saveNoteButton],
+        ),
+        backgroundColor: Colors.white70,
+        body: NoteScaffold(
+          child: newNote.newNoteTextField,
         ),
       ),
     );
