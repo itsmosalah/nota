76bc1227784c440876339eb40c6415fe70421dcb
mohamedabdelrahman <mohamed.abdelrahman@bld.ai>
2023-06-05 14:34:53 -0500

    Changed createNoteUI logic
    - Changed createNoteScreen to open the editor.
    - This screen is temporarily not in use.
    
    ref #8

--- nota/lib/screens/create_note_screen.dart
+++ nota/lib/screens/create_note_screen.dart
@@ -1,31 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:nota/components/new_note.dart';
-import 'package:nota/components/note_scaffold.dart';
 import 'package:nota/components/will_pop_scope_dialogue.dart';
+import 'package:nota/cubits/notes_data_cubit/cubit.dart';
+import 'package:nota/screens/edit_note_screen.dart';
 
 class CreateNoteScreen extends StatelessWidget {
   const CreateNoteScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    final NewNoteUI newNote = NewNoteUI(context: context);
+    // final NewNoteUI newNote = NewNoteUI(context: context);
+    final NotesDataCubit cubit = NotesDataCubit.get(context);
 
     return WillPopScopeDialogue(
-      header: 'Are you sure?',
-      textPrompt: 'Exiting will discard the changes.',
-      yesActionText: 'Discard',
-      noActionText: 'Continue editing',
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Create new note'),
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          actions: [newNote.saveNoteButton],
-        ),
-        backgroundColor: Colors.white70,
-        body: NoteScaffold(
-          child: newNote.newNoteTextField,
-        ),
-      ),
-    );
+        header: 'Are you sure?',
+        textPrompt: 'Exiting will discard the changes.',
+        yesActionText: 'Discard',
+        noActionText: 'Continue editing',
+        child: EditNoteScreen(note: cubit.createNewNote()));
   }
 }
