9db375190ce5012e3ef93ddee66884ffda442ed8
mohamedabdelrahman <mohamed.abdelrahman@bld.ai>
2023-06-05 20:55:28 -0500

    Created SQFLiteDB class
    - This class handles CRUD operations on notes
    
    ref #5

--- nota/lib/main.dart
+++ nota/lib/main.dart
@@ -4,13 +4,13 @@ import 'package:nota/cubits/notes_data_cubit/cubit.dart';
 import 'package:nota/screens/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
