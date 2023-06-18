1e61826a0625d16eaac9526687e190a26141bd98
mohamedabdelrahman <mohamed.abdelrahman@bld.ai>
2023-06-17 22:37:46 -0500

    Created SQL commands for creating required tables
    - Command for creating Labels table
    - Command for creating NotesLabels table
    
    ref #3

--- nota/lib/services/local/sqflite/sqflite_db.dart
+++ nota/lib/services/local/sqflite/sqflite_db.dart
@@ -3,26 +3,18 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class LocalDatabase {
-  late Future<String> _path;
-
-  LocalDatabase() {
-    initializeDBPath();
-  }
-
-  void initializeDBPath() async => _path = getDatabasesPath();
+  final Future<String> _path = getDatabasesPath();
 
   Future<void> deleteDatabase() async =>
       databaseFactory.deleteDatabase(await _path);
 
-  Future<Database> get database async
-      // Open the database and store the reference.
-      =>
-      openDatabase(
-        // Set the path to the database. Note: Using the `join` function from the
-        // `path` package is best practice to ensure the path is correctly
-        // constructed for each platform.
+  Future<Database> get database async => openDatabase(
         join(await _path, 'nota.db'),
-        onCreate: (db, version) => db.execute(SQLCommands.createNotesTable),
+        onCreate: (db, version) async {
+          await db.execute(SQLCommands.createNotesTable);
+          await db.execute(SQLCommands.createLabelsTable);
+          await db.execute(SQLCommands.createNotesLabelsTable);
+        },
         version: 1,
       );
 }
