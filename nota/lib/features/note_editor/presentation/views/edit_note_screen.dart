924ec0e9198a4b564030a7232f1ef03d2f05f62d
mohamedabdelrahman <mohamed.abdelrahman@bld.ai>
2023-07-04 13:49:52 -0500

    Add text editing toolbar
    - Create CustomQuillToolbar
    - Rearrange the widgets to place it below the NoteScaffold
    
    ref #11

--- nota/lib/features/note_editor/presentation/views/edit_note_screen.dart
+++ nota/lib/features/note_editor/presentation/views/edit_note_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart' hide Text;
 import 'package:nota/features/notes/views/widgets/note_scaffold.dart';
 import 'package:nota/features/notes/view_model/cubit.dart';
 import 'package:nota/features/note_editor/presentation/views/widgets/text_editing_widget.dart';
 import '../../../../core/models/note_model.dart';
+import 'widgets/custom_quill_toolbar.dart';
 
 class EditNoteScreen extends StatelessWidget {
   const EditNoteScreen({super.key, required this.note});
@@ -12,6 +15,11 @@ class EditNoteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final titleEditingController = TextEditingController(text: note.title);
+
+    final quillController = QuillController(
+      document: Document.fromJson(jsonDecode(note.content)),
+      selection: const TextSelection.collapsed(offset: 0),
+    );
 
     return WillPopScope(
       onWillPop: () async {
@@ -31,16 +39,36 @@ class EditNoteScreen extends StatelessWidget {
           ),
         ),
         backgroundColor: Colors.grey,
-        body: NoteScaffold(
-          child: Column(
-            children: [
-              Expanded(
-                child: TextEditingWidget(
-                  note: note,
+        body: Column(
+          children: [
+            Expanded(
+              child: NoteScaffold(
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: TextEditingWidget(
+                        note: note,
+                        quillController: quillController,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              color: Colors.deepPurple[100],
+              child: SingleChildScrollView(
+                clipBehavior: Clip.antiAlias,
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    CustomQuillToolbar(quillController: quillController)
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
